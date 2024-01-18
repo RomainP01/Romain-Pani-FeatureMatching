5952699b13aa3a5fc493039f7cd1b084d6fd42ad
Romain <romainpani01@gmail.com>
2024-01-18 15:30:56 +0100

    feat : adding project

---
+++ Romain.Pani.FeatureMatching.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using Romain.Pani.FeatureMatching;
+
+var objectImagePath = args[0];
+var scenesFolderPath = args[1];
+var objectImageData = await File.ReadAllBytesAsync(objectImagePath);
+var imageScenesData = new List<byte[]>();
+foreach (var imagePath in Directory.EnumerateFiles(scenesFolderPath))
+{
+    var imageBytes = await File.ReadAllBytesAsync(imagePath);
+    imageScenesData.Add(imageBytes);
+}
+
+var detectObjectInScenesResults = await new
+    ObjectDetection().DetectObjectInScenesAsync(objectImageData, imageScenesData);
+
+foreach (var objectDetectionResult in detectObjectInScenesResults)
+{
+    System.Console.WriteLine($"Points:{JsonSerializer.Serialize(objectDetectionResult.Points)}");
+}
+
+
+    
---
+++ Romain.Pani.FeatureMatching.Tests/FeatureMatchingUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using Xunit;
+
+namespace Romain.Pani.FeatureMatching.Tests;
+
+public class FeatureMatchingUnitTest
+{
+    [Fact]
+    public async Task ObjectShouldBeDetectedCorrectly()
+    {
+        var executingPath = GetExecutingPath();
+        var imageScenesData = new List<byte[]>();
+        foreach (var imagePath in Directory.EnumerateFiles(Path.Combine(executingPath,
+                     "Scenes")))
+        {
+            var imageBytes = await File.ReadAllBytesAsync(imagePath);
+            imageScenesData.Add(imageBytes);
+        }
+
+        var objectImageData = await File.ReadAllBytesAsync(Path.Combine(executingPath,
+            "Pani-Romain-object.jpg"));
+        var detectObjectInScenesResults = await new
+            ObjectDetection().DetectObjectInScenesAsync(objectImageData, imageScenesData);
+
+        Assert.Equal("[{\"X\":1864,\"Y\":184},{\"X\":1165,\"Y\":1253},{\"X\":1857,\"Y\":1769},{\"X\":2639,\"Y\":767}]",
+            JsonSerializer.Serialize(detectObjectInScenesResults[0].Points));
+
+        Assert.Equal("[{\"X\":1864,\"Y\":1306},{\"X\":2018,\"Y\":870},{\"X\":1461,\"Y\":-157},{\"X\":1451,\"Y\":1757}]",
+            JsonSerializer.Serialize(detectObjectInScenesResults[1].Points));
+    }
+
+    private static string GetExecutingPath()
+    {
+        var executingAssemblyPath = Assembly.GetExecutingAssembly().Location;
+        var executingPath = Path.GetDirectoryName(executingAssemblyPath);
+        return executingPath;
+    }
+}
